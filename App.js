022b6b16ac677397c73951fa9b3376c3eabcd14c
AngelJTumG <javierangel43.lo@gmail.com>
2025-06-10 00:30:43 -0600

    test: error de dependencias

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
-
+import { NavigationContainer } from '@react-navigation/native';
 import MainStackNavigator from './navigation/MainNavigate';
 
 export default function App() {
   return (
-    <>
+    <NavigationContainer>
       <MainStackNavigator />
-    </>
+    </NavigationContainer>
   );
 }
 
--- navigation/MainNavigate.js
+++ navigation/MainNavigate.js
@@ -1,20 +1,18 @@
-import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import HomeScreen from "../screens/HomeScreen";
-import ProductDetailsScreen from "../screens/ProductDetailsScreenn";
+import ProductDetailsScreen from "../screens/ProductDetailsScreen";
 import CartScreen from "../screens/CartScreen";
 
 const Stack = createStackNavigator();
 
+// NO NavigationContainer aquí, solo el Stack.Navigator
 const MainStackNavigator = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="ProductDetails" component={ProductDetailsScreen} />
-        <Stack.Screen name="cart" component={CartScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="ProductDetails" component={ProductDetailsScreen} />
+      <Stack.Screen name="cart" component={CartScreen} />
+    </Stack.Navigator>
   );
 };
 
